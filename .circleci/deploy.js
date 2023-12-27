b3e6f3efbccb05c008371a7b8e818b30e258877e
Nicolas Fetter <nicofetter@gmail.com>
2023-12-27 13:15:31 -0300

    Adding circleci config

---
+++ .circleci/deploy.js
@@ -0,0 +1,23 @@
+const FtpDeploy = require('ftp-deploy')
+
+const ftpDeploy = new FtpDeploy()
+
+const config = {
+  user: process.env.FTPUSERNAME,
+  password: process.env.FTPPASS,
+  host: process.env.FTPHOST,
+  port: 21,
+  localRoot: `${__dirname}/../public/`,
+  remoteRoot: '/',
+  include: ['*'],
+}
+
+ftpDeploy.deploy(config, (err) => {
+  if (err) {
+    console.log(err)
+    throw new Error(err)
+  } else {
+    console.log('finished')
+  }
+})
+
